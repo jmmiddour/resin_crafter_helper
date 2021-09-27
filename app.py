0f2938dcd58d2d7528034d14272f9c246c334ecd
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-09-27 01:25:19 -0400

    trying to fix error on deployed version where it does not hold login information

--- app.py
+++ app.py
@@ -49,8 +49,7 @@ def after_request(response):
 # #   which is done by default with Flask
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = True
-app.config["SESSION_SQLALCHEMY"] = True
-app.config["SESSION_TYPE"] = "sqlalchemy"
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
